d367ecf6240cb9974310c9754e2aa60cd0d81a4a
unknown <fatma.chahbar@gmail.com>
2023-01-05 22:37:01 +0100

    NavBar styling

---
+++ src/NavBar.js
@@ -0,0 +1,10 @@
+import react from 'react'
+import logo from './Images/airbnb1.png';
+ export default function NavBar(){
+    return(
+        <nav>
+            
+        <img src={logo}/>
+        </nav>
+    )
+ }
